aca3871e68520510d19cb738a38bd4f6fc657fc0
gbye <gbyewolf@gmail.com>
2021-01-29 14:27:16 +0800

    add movieifo

--- vuecli-netflix/src/App.vue
+++ vuecli-netflix/src/App.vue
@@ -1,32 +1,39 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <movie-info title="Original" :url="baseUrl" :req="requests.fetchNetflixOriginals" />
+    <movie-info title="ActionMovies" :url="baseUrl" :req="requests.fetchActionMovies" />
   </div>
 </template>
 
+<script>
+  import MovieInfo from "./components/MovieInfo.vue";
+  const baseUrl= "https://api.themoviedb.org/3";
+  const m = "a54618fe0fbb1c35737e9a35ec28e5de";
+  const requests = {
+    fetchTrending: "/trending/all/week?api_key=".concat(m, "&language=en-US"),
+    fetchNetflixOriginals: "/discover/tv?api_key=".concat(m, "&with_network=213"),
+    fetchTopRated: "/movie/top_rated?api_key=".concat(m, "&language=en-US"),
+    fetchActionMovies: "/discover/movie?api_key=".concat(m, "&with_genres=28"),
+    fetchComedyMovies: "/discover/movie?api_key=".concat(m, "&with_genres=35"),
+    fetchHorrorMovies: "/discover/movie?api_key=".concat(m, "&with_genres=27"),
+    fetchRomanceMovies: "/discover/movie?api_key=".concat(m,"&with_genres=10749"),
+    fetchDocumentaries: "/discover/movie?api_key=".concat(m, "&with_genres=99"),
+  };
+  export default {
+    name: "App",
+    data() {
+      return {
+        requests:requests,
+        baseUrl:baseUrl
+      };
+    },
+    components: {
+      MovieInfo,
+    },
+  };
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  html{
+    overflow-x: hidden;
+  }
 </style>
---
+++ vuecli-netflix/src/components/MovieInfo.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="info">
+    <h1>{{ title }}</h1>
+    <ul @mousewheel="scroll">
+      <li v-for="item in infos" :key="item.id">
+        <img :src="src + item.poster_path" alt="" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "MovieInfo",
+    data() {
+      return {
+        infos: [],
+        src: "https://image.tmdb.org/t/p/w500",
+      };
+    },
+    mounted() {
+      this.$axios.get(this.url + this.req).then((res) => {
+        this.infos = res.data.results;
+      });
+    },
+    props: {
+      url: String,
+      req: String,
+      title: String,
+    },
+    methods: {
+      scroll(e) {
+        let width = document.querySelector("li img").width;
+        let ul = document.querySelector("ul");
+        let left = parseInt(document.querySelector("ul").style.left) || 0;
+        if (e.wheelDelta == 120) {
+          if (left == 0) {
+            return false;
+          } else {
+            ul.style.left = `${left + width}px`;
+          }
+        } else if (e.wheelDelta == -120) {
+          if (Math.abs(left) == width * 10) {
+            return false;
+          } else {
+            ul.style.left = `${left - width}px`;
+          }
+        }
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  img {
+    max-width: 100%;
+  }
+  .info {
+    position: relative;
+    height: 200px;
+  }
+  .info ul {
+    list-style: none;
+    padding: 0;
+    overflow-x: hidden;
+    overflow-y: hidden;
+    display: grid;
+    grid-auto-flow: column;
+    position: absolute;
+    left: 0;
+    transition: 0.8s;
+  }
+  .info li {
+    display: block;
+    margin-right: 10px;
+    width: 10vw;
+    transition: 0.5s;
+  }
+  .info li:hover {
+    transform: scale(1.05);
+  }
+</style>
