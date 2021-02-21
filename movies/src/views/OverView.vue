f888ee3933da58265912e3d655166179eb9fc528
kcc <kcc7907@gmail.com>
2021-02-21 21:45:02 +0800

    update overview.vue

--- movies/src/views/OverView.vue
+++ movies/src/views/OverView.vue
@@ -13,7 +13,7 @@
           <h3 class="movieTitle text-center">{{ title }}</h3>
           <div class="facts"></div>
           <h5>電影簡介</h5>
-          <p>{{ datas.overview?datas.overview:empty }}</p>
+          <p>{{ datas.overview ? datas.overview : empty }}</p>
           <h5>電影演員</h5>
           <ul class="cast d-flex p-0">
             <li
@@ -41,105 +41,139 @@
     <iframe
       class="container-fluid"
       :height="embedHeight"
-      src=""
+      :src="ytEmbed"
       frameborder="0"
     ></iframe>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "OverView",
-    props: ["id", "title"],
-    data() {
-      return {
-        datas: [],
-        srcOriginal: this.$store.state.src,
-        srcSmall: this.$store.state.imgSrc,
-        baseUrl: this.$store.state.baseUrl,
-        embedHeight: 0,
-        cast:[],
-        iframe:'',
-        empty:'目前暫無簡介，請等待更新'
-      };
+export default {
+  name: "OverView",
+  props: ["id", "title"],
+  data() {
+    return {
+      datas: [],
+      srcOriginal: this.$store.state.src,
+      srcSmall: this.$store.state.imgSrc,
+      baseUrl: this.$store.state.baseUrl,
+      embedHeight: 0,
+      cast: [],
+      iframe: "",
+      empty: "目前暫無簡介，請等待更新",
+      ytID: "",
+      video: "",
+    };
+  },
+  computed: {
+    movieApi() {
+      //https://api.themoviedb.org/3/movie/157336?api_key={api_key}&append_to_response=videos
+      return `https://api.themoviedb.org/3/movie/${this.id}?api_key=a54618fe0fbb1c35737e9a35ec28e5de&language=zh-TW&append_to_response=videos,images,credits`;
     },
-    computed: {
-      movieApi() {
-        //https://api.themoviedb.org/3/movie/157336?api_key={api_key}&append_to_response=videos
-        return `https://api.themoviedb.org/3/movie/${this.id}?api_key=a54618fe0fbb1c35737e9a35ec28e5de&language=zh-TW&append_to_response=videos,images,credits`;
-      },
-      ytSearch() {
-        return `https://www.googleapis.com/youtube/v3/search?&key=AIzaSyD2y-s2porfmEKilPnh-0rFp1MADnu1Tiw&maxResults=1&q=${this.title} 預告`;
-      },
-      ytEmbed() {
-        return `https://www.youtube.com/embed/`;
-      },
-      // catsData(){
-      //   let cast;
-      //   if(this.datas.credits.cast){
-      //     cast = this.datas.credits.cast.slice(0, 20)
-      //   }else{
-      //     return false
-      //   }
-      //   return cast
-      // }
+    videoApi() {
+      return `https://api.themoviedb.org/3/movie/${this.id}?api_key=a54618fe0fbb1c35737e9a35ec28e5de&language=en-US&append_to_response=videos`;
     },
-    methods: {
-      getOverView() {
-        this.$axios.get(this.movieApi)
+    ytSearch() {
+      return `https://www.googleapis.com/youtube/v3/search?&key=AIzaSyD2y-s2porfmEKilPnh-0rFp1MADnu1Tiw&maxResults=1&q=${this.title} 預告`;
+    },
+    ytEmbed() {
+      return `https://www.youtube.com/embed/${this.video}`;
+    },
+  },
+  methods: {
+    getOverView() {
+      this.$axios
+        .get(this.movieApi)
         .then((res) => {
           this.datas = res.data;
         })
-        .then(res=>{
+        .then(() => {
           this.cast = this.datas.credits.cast.slice(0, 20);
-          this.iframe = this.datas.videos.results
+          this.iframe = this.datas.videos.results;
         });
-      },
-      widowSizeCheck() {
-        // if(window.innerWidth < 900){
-        // }else {
-        // }
-      },
     },
-    mounted() {
-      // window.onresize=()=>{
-      //   if(window.innerWidth < 900){
-      //     this.test = 100
-      //     this.widowSizeCheck();
-      //   }
+    widowSizeCheck() {
+      // if(window.innerWidth < 900){
+      // }else {
       // }
-      this.getOverView();
+      if (this.video !== 0 && this.video) {
+        console.log(this.video);
+        // if (window.innerWidth < 900) {
+        // }
+      }
     },
-  };
+    getYTID() {
+      return this.$axios
+        .get(this.ytSearch)
+        .then((res) => {
+          this.ytID = res.data.items.id.videoId;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getVideo() {
+      return this.$axios.get(this.videoApi).then((res) => {
+        this.video = res.data.videos.results[0].key;
+      });
+    },
+  },
+  mounted() {
+    // window.onresize=()=>{
+    //   if(window.innerWidth < 900){
+    //     this.test = 100
+    //     this.widowSizeCheck();
+    //   }
+    // }
+    this.getOverView();
+    this.getVideo();
+    this.widowSizeCheck();
+
+    // (async () => {
+    //   await this.getYTID();
+    //   await  (function(){
+    //     return  new Promise((res) => {
+    //     res();
+    //   });
+    //   })();
+    // })();
+  },
+  watch: {
+    embedHeight() {
+      console.log("watch");
+      this.widowSizeCheck();
+    },
+  },
+};
 </script>
 
 <style lang="scss" >
-  * {
-    color: white;
-  }
-  .overview {
-    margin-top: 10px;
-  }
-  ul.cast {
-    width: 100%;
-    overflow: auto;
+* {
+  color: white;
+}
+.overview {
+  margin-top: 10px;
+}
+ul.cast {
+  width: 100%;
+  overflow: auto;
+  list-style: none;
+  text-align: center;
+  margin-bottom: 10px;
+  li.actor {
+    margin-right: 10px;
     list-style: none;
-    text-align: center;
-    margin-bottom: 10px;
-    li.actor {
-      margin-right: 10px;
-      list-style: none;
-      padding: 0;
-      border-radius: 8px;
-      margin-bottom: 8px;
-    }
-    p {
-      margin: 15px 0 0;
-    }
-  }
-  .images {
-    overflow: hidden;
+    padding: 0;
     border-radius: 8px;
-    width: 150px;
+    margin-bottom: 8px;
   }
+  p {
+    margin: 15px 0 0;
+  }
+}
+.images {
+  overflow: hidden;
+  border-radius: 8px;
+  width: 150px;
+}
 </style>
